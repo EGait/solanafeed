43b55bda6b53dd2118fbff409ea28e7894d698da
Erin Gaither <eringaither22@gmail.com>
2026-04-11 18:07:21 -0500

    fix sw

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,42 @@
-const CACHE_NAME = 'solanafeed-v2'
+const CACHE_NAME = 'solanafeed-v3';
 const urlsToCache = [
   '/',
-]
+  '/projects',
+  '/news',
+  '/lsts',
+  '/stablecoins',
+  '/swap',
+  '/about',
+];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting()
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache)
+      return cache.addAll(urlsToCache);
     })
-  )
-})
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request)
-    })
-  )
-})
+  );
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      )
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
-  )
-})
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
