c0b9f638eee6e3d2e151b9fb4f5b4e0860ef6a5d
Erin Gaither <eringaither22@gmail.com>
2026-04-11 15:54:29 -0500

    revert turbopack and tailwind changes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+<<<<<<< HEAD
 const nextConfig: NextConfig = {};
+=======
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+>>>>>>> parent of 02329f5 (add tailwind config)
 
 export default nextConfig;
