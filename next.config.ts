cd4a29b48647e0aa0b3cd3ccec0849d485321590
Erin Gaither <eringaither22@gmail.com>
2026-04-11 15:56:58 -0500

    revery to working

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-<<<<<<< HEAD
-const nextConfig: NextConfig = {};
-=======
 const nextConfig: NextConfig = {
   /* config options here */
 };
->>>>>>> parent of 02329f5 (add tailwind config)
 
 export default nextConfig;
